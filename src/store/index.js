e9ca6e8a23597a7d11d2e51f266739d170be7d72
huabingtao <1273621932@qq.com>
2018-04-02 15:15:31 +0800

    删除额外的项目代码

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,9 @@ import Vuex from 'vuex'
 import * as actions from './actions'
 import * as getters from './getters'
 import  mutations from './mutations'
-// import state from './state'
+import state from './state'
 import * as func from '../common/js/local';
 Vue.use(Vuex)
-
-const state = func.local.get() || {
-    event: [],
-    count: 0
-}
 
 export default new Vuex.Store({
   actions,
--- src/store/state.js
+++ src/store/state.js
@@ -1,1 +1,6 @@
-
+import * as func from '../common/js/local'
+const state = func.local.get() || {
+    event: [],
+    count: 0
+}
+export default state
