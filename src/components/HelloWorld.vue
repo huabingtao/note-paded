fea112a9fcf786018a625b74a38d35e050e72897
hbt <hbt@hbtdeMacBook-Pro.local>
2019-04-10 13:13:54 +0800

    push master

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,6 +17,7 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    
     <cube-button @click="showToast">show toast</cube-button>
   </div>
 </template>
